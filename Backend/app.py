01d065403f7e956491eb54dfb29fefc881715acf
Smasia <sander130506@gmail.com>
2024-05-01 15:20:08 +0200

    La til logg inn side som sjekker om bruker er i database, og sender brukeren til hjemme siden hvis bruker er i databasen

--- Backend/app.py
+++ Backend/app.py
@@ -3,22 +3,42 @@ import sqlite3
 
 app = Flask(__name__)
 
-con = sqlite3.connect("database.db")
+con = sqlite3.connect("database.db", check_same_thread=False)
 cur = con.cursor()
 
 cur.execute("""
             CREATE TABLE IF NOT EXISTS brukere(
-            id int primary key,
+            id integer primary key,
             navn text
             )
             """)
 cur.execute("""
             CREATE TABLE IF NOT EXISTS todo(
-            id int primary key,
-            bruker_id int,
+            id integer primary key,
+            bruker_id integer,
             task text
             )
             """)
+
+cur.execute("DELETE from brukere")
+
+brukere = ["Sander", "Kjell", "Harald"]
+
+cur.executemany("""
+            INSERT INTO brukere(navn)
+            VALUES(?)
+            """, [(navn,) for navn in brukere])
+con.commit()
+
+@app.route("/logg_inn", methods = ["GET"])
+def logg_inn():
+  navn = request.get_json()["navn"]
+  cur.execute("SELECT * from brukere WHERE navn = ?", (navn,))
+  content = cur.fetchone()
+  if content == None:
+    return {"Status": "Not a user"}
+  else:
+    return {"Status": "A user", "bruker_id": content[0], "bruker_navn": content[1]}
 
 if __name__ == "__main__":
   app.run(debug=True, port=5020)
--- Frontend/app.py
+++ Frontend/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
-def home_page():
+def index():
   return render_template('index.html')
 
+@app.route("/logg_inn", methods = ["POST"])
+def logg_inn():
+  navn = request.form.get("fornavn")
+  print(navn)
+  content = requests.get('http://127.0.0.1:5020/logg_inn', json={"navn": navn}).json()
+  if content["Status"] == "A user":
+    return redirect(url_for('home_page', bruker_navn=content["bruker_navn"]))
+  return render_template("index.html", feil="Fant ikke bruker i systemet")
+
+@app.route("/home_page/<bruker_navn>", methods = ["GET"])
+def home_page(bruker_navn):
+  
+  return render_template("home.html")
 
 if __name__ == '__main__':
   app.run(debug=True, port=5010)
